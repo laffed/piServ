404957c92536fe375e7df3e1def8a60a4385de97
laffed <roarklaffed@gmail.com>
2021-05-19 20:35:33 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import clocker from './clocker';
 
 const app = express();
 const port = 8080;
@@ -11,11 +10,6 @@ app.use(express.json());
 
 app.get('/', (req, res, next) => {
   res.send('The sedulous hyena ate the antelope!');
-});
-
-app.get('/status', async (req, res, next) => {
-  clocker();
-  res.send('Oh yeah baby')
 });
 
 app.listen(port, () => {
